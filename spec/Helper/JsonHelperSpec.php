9d0b042e4675bd067454dcaaf86e53b4b1c1a6ce
fesor <fesors@gmail.com>
2014-11-29 01:25:03 +0300

    Clear specs

--- spec/Helper/JsonHelperSpec.php
+++ spec/Helper/JsonHelperSpec.php
@@ -9,38 +9,38 @@ use Seld\JsonLint\JsonParser;
 class JsonHelperSpec extends ObjectBehavior
 {
 
-    public function let()
+    function let()
     {
         $this->beConstructedWith(new JsonParser());
     }
 
-    public function it_parses_json()
+    function it_parses_json()
     {
         $result = new \stdClass();
         $result->json = ['spec'];
         $this->parse('{"json":["spec"]}')->shouldBeLike($result);
     }
 
-    public function it_parses_JSON_values()
+    function it_parses_JSON_values()
     {
         $this->parse('"json_spec"')->shouldBe('json_spec');
         $this->parse('10')->shouldBe(10);
         $this->parse('null')->shouldBe(null);
     }
 
-    public function it_raises_a_parser_error_for_invalid_JSON()
+    function it_raises_a_parser_error_for_invalid_JSON()
     {
         $this->shouldThrow()->duringParse('json_spec');
     }
 
-    public function it_parses_at_a_path_if_given()
+    function it_parses_at_a_path_if_given()
     {
         $json = '{"json": ["spec"]}';
         $this->parse($json, 'json')->shouldBeLike(["spec"]);
         $this->parse($json, 'json/0')->shouldBe('spec');
     }
 
-    public function it_raises_an_error_for_a_missing_path()
+    function it_raises_an_error_for_a_missing_path()
     {
         $json = '{"json": ["spec"]}';
         $this->shouldThrow(
@@ -48,19 +48,19 @@ class JsonHelperSpec extends ObjectBehavior
         )->duringParse($json, 'json/1');
     }
 
-    public function it_parses_at_a_numeric_string_path()
+    function it_parses_at_a_numeric_string_path()
     {
         $json = '{"1": "json"}';
         $this->parse($json, '1')->shouldBe('json');
     }
 
-    public function it_correctly_validate_json_value()
+    function it_correctly_validate_json_value()
     {
         $this->isValid('"json_spec"')->shouldBe(true);
         $this->isValid('json_spec')->shouldBe(false);
     }
 
-    public function it_normalize_json()
+    function it_normalize_json()
     {
         $normalizedJson = <<<EOD
 {
@@ -75,27 +75,27 @@ EOD;
         $this->normalize('{"laser":{"lemon": "orange", "banana": "watermelon"},"json":"spec"}')->shouldBe(rtrim($normalizedJson));
     }
 
-    public function it_normalize_json_value()
+    function it_normalize_json_value()
     {
         $this->normalize('1e+1')->shouldBe('10');
     }
 
-    public function it_normalizes_at_a_path()
+    function it_normalizes_at_a_path()
     {
         $this->normalize('{"json":["spec"]}', "json/0")->shouldBe('"spec"');
     }
 
-    public function it_accept_a_json_value()
+    function it_accept_a_json_value()
     {
         $this->normalize('1e+1')->shouldBe('10');
     }
 
-    public function it_normalizes_a_json_value()
+    function it_normalizes_a_json_value()
     {
         $this->normalize('"json_spec"')->shouldBe('"json_spec"');
     }
 
-    public function it_does_not_change_collection_order()
+    function it_does_not_change_collection_order()
     {
         $normalizedJson = <<<EOD
 [
@@ -107,7 +107,7 @@ EOD;
         $this->generateNormalizedJson(['spec', 'json'])->shouldBe(rtrim($normalizedJson));
     }
 
-    public function it_generates_a_normalized_json_document()
+    function it_generates_a_normalized_json_document()
     {
         $normalizedJson = <<<EOD
 {
@@ -119,7 +119,7 @@ EOD;
         $this->generateNormalizedJson((object) ['json'=>['spec']])->shouldBe(rtrim($normalizedJson));
     }
 
-    public function it_should_exclude_keys()
+    function it_should_exclude_keys()
     {
         $data = (object) array(
             'id' => 1,
