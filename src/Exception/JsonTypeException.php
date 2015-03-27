4b9d9a40d7a3d2f7c84d54fec33d8cde6fb1c6cf
Sergey Protko <fesors@gmail.com>
2015-03-27 23:01:35 +0300

    Fix typo

--- src/Exception/JsonTypeException.php
+++ src/Exception/JsonTypeException.php
@@ -16,7 +16,7 @@ class JsonTypeException extends MatchException
     public static function create($expectedType, $actualType, array $options)
     {
         if (self::isPositive($options)) {
-            $message = sprintf('Expected JSON value size to be %s, but got %s%s', $expectedType, $actualType, self::getAt($options));
+            $message = sprintf('Expected JSON value type to be %s, but got %s%s', $expectedType, $actualType, self::getAt($options));
         } else {
             $message = sprintf('Expected JSON value type to not be %s%s', $expectedType, self::getAt($options));
         }
